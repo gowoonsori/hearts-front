450fc765279d53abb23b14539aa94673df032c52
gowoonsori <gowoonsori97@naver.com>
2021-07-28 21:32:19 +0900

    Fix : 좋아요시 태그가 배열형태로 바뀌는 문제

--- src/components/boxes/PostBox.js
+++ src/components/boxes/PostBox.js
@@ -1,6 +1,6 @@
 import { Avatar, Box, Container, Typography } from '@material-ui/core';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import {user} from '../../atoms/user';
+import { user } from '../../atoms/user';
 import TagList from '../lists/TagList';
 import MoreButton from '../buttons/MoreButton';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -19,7 +19,9 @@ const PostBox = ({ post }) => {
   const [likeList, setLikeList] = useRecoilState(likes);
   const [postList, setPostList] = useRecoilState(posts);
   const axios = useRecoilValue(instance);
-  
+  const tags = typeof post.tags === 'string' ? JSON.parse(post.tags) : post.tags;
+
+  console.log();
   const likeEvent = useCallback(async () => {
     const res = await axios.patch(`/user/post/${post.id}/like`).catch((e) => console.log(2));
     if (res?.data.success) {
@@ -27,7 +29,7 @@ const PostBox = ({ post }) => {
       const newLikeList = likeList.slice(0, likeList.length);
       newLikeList.push(post.id);
       setLikeList(newLikeList);
-      
+
       //기존의 postList atom 내용 업데이트
       const newPostList = postList.slice(0, postList.length);
       const index = newPostList.findIndex((e) => e.id == post.id);
@@ -35,14 +37,14 @@ const PostBox = ({ post }) => {
       setPostList(newPostList);
     }
   }, [likeList, setLikeList, postList, setPostList]);
-  
+
   const deleteLikeEvent = useCallback(async () => {
     const res = await axios.delete(`/user/post/${post.id}/like`).catch((e) => console.log(2));
     if (res?.data.success) {
       //좋아요 atom 업데이트
       const newLikeList = likeList.filter((like) => like != post.id);
       setLikeList(newLikeList);
-      
+
       //기존의 postList atom 내용 업데이트
       const newPostList = postList.slice(0, postList.length);
       const index = newPostList.findIndex((e) => e.id == post.id);
@@ -50,22 +52,27 @@ const PostBox = ({ post }) => {
       setPostList(newPostList);
     }
   }, [likeList, post, setLikeList, postList, setPostList]);
-  
-  const liked = useCallback(()=> {
-    return likeList.find((v) => v == post.id)
-  },[likeList]);
+
+  const liked = useCallback(() => {
+    return likeList.find((v) => v == post.id);
+  }, [likeList]);
+
   return (
     <Container id={post.id} sx={{ borderRadius: '5px', display: 'block', mx: 0, my: 1, py: 2, px: 4, backgroundColor: 'primary.main', color: 'secondary.main' }}>
-      <Box sx={{ display: 'flex', mb:5 }}>
-        <Avatar/>
-        <Typography sx={{margin:'auto 0',mx:2}} variant="h3">{post.owner}</Typography>
-        <Typography variant="h6" sx={{margin:'auto 0',color:"primary.contrastText"}}>{post.category}</Typography>
+      <Box sx={{ display: 'flex', mb: 5 }}>
+        <Avatar />
+        <Typography sx={{ margin: 'auto 0', mx: 2 }} variant="h3">
+          {post.owner}
+        </Typography>
+        <Typography variant="h6" sx={{ margin: 'auto 0', color: 'primary.contrastText' }}>
+          {post.category}
+        </Typography>
       </Box>
       <Box sx={{ display: 'inline' }}>
         <Typography variant="h2">{post.content}</Typography>
       </Box>
-      <TagList tags={JSON.parse(post.tags)} />
-      <Box sx={{ width: '100%', display: 'inline-flex', justifyContent: 'space-between', mt:2 }}>
+      <TagList tags={tags} />
+      <Box sx={{ width: '100%', display: 'inline-flex', justifyContent: 'space-between', mt: 2 }}>
         {liked() ? <PostUtilBox Icon={FavoriteIcon} text={post.total_like} onClick={deleteLikeEvent} /> : <PostUtilBox Icon={FavoriteBorderIcon} text={post.total_like} onClick={likeEvent} />}
         <PostUtilBox Icon={ShareIcon} text={post.share_cnt} />
         <CopyBox id={post.id} Icon={FileCopyIcon} content={post.content} />
--- src/components/lists/TagList.js
+++ src/components/lists/TagList.js
@@ -3,13 +3,7 @@ import PropTypes from 'prop-types';
 import TagBox from '../boxes/TagBox';
 
 const TagList = ({ tags }) => {
-  return (
-    <Box sx={{ display: 'inline-flex', p: 0, m: 2, ml: 0, mb: 1 }}>
-      {tags && tags.map((tag, index) => (
-        <TagBox key={index} tag={tag} />
-      ))}
-    </Box>
-  );
+  return <Box sx={{ display: 'inline-flex', p: 0, m: 2, ml: 0, mb: 1 }}>{tags.length > 0 && tags.map((tag, index) => <TagBox key={index} tag={tag} />)}</Box>;
 };
 
 TagList.propTypes = {
